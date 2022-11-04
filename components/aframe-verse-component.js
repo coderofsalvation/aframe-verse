aea1b5838b8cd3a08bfb9e5b09c397bf67ff618e
ci-appveyor <CI@appveyor.com>
2022-11-04 09:43:22 +0100

    cleanup

--- components/aframe-verse-component.js
+++ components/aframe-verse-component.js
@@ -4,13 +4,13 @@ AFRAME.registerComponent('href', {
   getVerse: function(){
     return this.el.components["aframe-verse"] || this.el.closest('[aframe-verse]').components["aframe-verse"]
   }, 
-  emitPromise: function(e, opts){ // emitting promises instead of data allows more control for listeners
+  emitPromise: function(e, opts, averse){ // emitting promises instead of data allows more control for listeners
     return new Promise( (resolve, reject) => {
       opts.promise = () => {
         opts.promise.halted = true
         return { resolve, reject }
       }
-      this.getVerse().el.emit(e, opts)     
+      (averse || this.getVerse()).el.emit(e, opts)     
       if( !opts.promise.halted ) resolve()
     })
   }, 
@@ -34,7 +34,7 @@ AFRAME.registerComponent('href', {
 
       this.emitPromise('beforeNavigate', ctx)
           .then( () => {
-            if( averse.data.fade != 0 ) $('[fadebox]').components.fadebox.in( navigate )
+            if( averse.data.fade != 0 ) $('[fader]').components.fader.in( navigate )
             else navigate()
           })
     }
@@ -56,15 +56,18 @@ AFRAME.registerComponent('href', {
     .then( (res ) => res.text() )
     .then( (html) => new window.DOMParser().parseFromString(html, "text/html") )
     .then( (dom ) => {
+      dest.dom = dom
       if( !gohome ) averse.setBaseHref( dest.url )
-      this.emitPromise('loadHTML',{destination:dest, dom, gohome})
-          .then( () =>  averse.el.innerHTML = dom.querySelector('[aframe-verse]').innerHTML )
-          .catch( console.warn ) 
+      return this.emitPromise('loadHTML',{destination:dest, gohome})
     })
+    .then( () =>  averse.el.innerHTML = dest.dom.querySelector('[aframe-verse]').innerHTML )
     .catch( (e) => console.error(e) )
     .finally( () => {
-      averse.loading = false
-      if( averse.data.fade != 0 ) $('[fadebox]').components.fadebox.out()
+      this.emitPromise('loaded',{destination:dest,gohome},averse)
+      .then( () => {
+        averse.loading = false
+        if( averse.data.fade != 0 ) $('[fader]').components.fader.out()
+      })
     })
   } 
 
@@ -75,8 +78,9 @@ AFRAME.registerComponent('aframe-verse', {
   schema:{
     debug:      {type:"boolean", "default":false}, 
     register:   {type:"string"}, 
+    hrefEvents: {type:"array", "default":["click","collide"]},
     fade:       {type:"number", "default":1000}, 
-    hrefEvents: {type:"array", "default":["click"]}
+    fadeColor:  {type:"string"}
   },
 
   registerJSON: function(url){
@@ -126,8 +130,8 @@ AFRAME.registerComponent('aframe-verse', {
 
   initCamera: function(){
     let cam = $('[camera]')
-    if( !cam.getAttribute('fadebox') ){
-      cam.setAttribute("fadebox", `fadetime: ${this.data.fade}; `+$('a-scene').getAttribute("background") )
+    if( !cam.getAttribute('fader') ){
+      cam.setAttribute("fader",`fadetime: ${this.data.fade}; color: ${this.data.fadeColor||'black'}`);
       cam.setAttribute("mouse-cursor", {})
     }
     return this
@@ -146,29 +150,31 @@ AFRAME.registerComponent('aframe-verse', {
 
 });
 
-AFRAME.registerComponent('fadebox', {
+AFRAME.registerComponent('fader', {
   schema:{
     fadetime:{type:"number"}, 
-    color:{type:"color"}, 
+    color:{type:"color","default":"black"}, 
+    entity:{type:"selector"}, 
   },
   init: function(){
-    let fb = this.fb = document.createElement("a-box")
-    fb.setAttribute("scale", "0.5 0.5 0.5")
-    fb.setAttribute("material", `color: ${this.data.color}; transparent: true; side: back; shader: flat`)
-    this.el.appendChild(fb)
+    let f = this.fader = this.data.entity ? this.data.entity : document.createElement("a-box")
+    f.setAttribute("scale", "0.5 0.5 0.5")
+    f.setAttribute("material", `color: ${this.data.color}; transparent: true; side: back; shader: flat`)
+    console.log(`color: ${this.data.color}; transparent: true; side: back; shader: flat`)
+    this.el.appendChild(f)
     this.out()
   }, 
   out: function(cb){
-    this.fb.setAttribute("animation", `property: components.material.material.opacity; dur: ${this.data.fadetime}; from: 1; to: 0`)
+    this.fader.setAttribute("animation", `property: components.material.material.opacity; dur: ${this.data.fadetime}; from: 1; to: 0`)
 		clearTimeout(this.tid)
 		this.tid = setTimeout( (cb) => {
-      this.fb.setAttribute("visible",false)
+      this.fader.setAttribute("visible",false)
       if( cb ) cb()
     }, this.data.fadetime*1.2, cb ) 
   }, 
   "in": function(cb){
-    this.fb.setAttribute("animation", `property: components.material.material.opacity; dur: ${this.data.fadetime}; from: 0; to: 1`)
-    this.fb.setAttribute("visible",true)
+    this.fader.setAttribute("animation", `property: components.material.material.opacity; dur: ${this.data.fadetime}; from: 0; to: 1`)
+    this.fader.setAttribute("visible",true)
     if( cb ) setTimeout( cb, this.data.fadetime*1.2 )
   }
 });
